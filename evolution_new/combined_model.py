aadacdd3e6d810b66014aa178fbef199e8e93be2
Laiv el Vyasa <felix@mindt-online.de>
2023-02-28 23:36:54 +0100

    Combined Model forward added

--- evolution_new/combined_model.py
+++ evolution_new/combined_model.py
@@ -3,7 +3,10 @@ from pygad import pygad
 from pygad import torchga
 import pygad.torchga
 
+from multiprocessing import Pool
 
+from evolution_utils import get_edge_data, get_diagonal_of_qubo, get_tensor_of_structure, apply_approximation_to_qubo, \
+    linearize_qubo
 from learning_model import LearningModel
 from neural_networks import CombinedNodeFeaturesUwu, CombinedEdgeDecisionUwu
 
@@ -25,10 +28,43 @@ class CombinedModel(LearningModel):
         # Get the cutoff point, where the pygad string has to be sliced
         self.node_edge_cutoff = sum(p.numel() for p in self.node_model.parameters() if p.requires_grad)
 
-    def get_approximation(self, qubo_list, problem_list):
-        pass
+    def get_approximation(self, qubo_list: list, problem_list: list) -> list:
+        approxed_qubo_list = [None for _ in qubo_list]
+        with Pool() as pool:
+            argument_list = [qubo_list, problem_list, [i for i in range(len(qubo_list))]]
+            for approxed_qubo, index in pool.starmap(self.get_approxed_qubo, argument_list):
+                approxed_qubo_list[index] = approxed_qubo
+        return approxed_qubo_list
 
-    def set_model_weights_from_pygad(self, pygad_chromosome):
+    def get_approxed_qubo(self, qubo, problem, index):
+        edge_index, edge_weight = get_edge_data(qubo)
+        node_model, node_features = self.get_node_model_and_features(problem, qubo)
+        node_features = node_model.forward(get_tensor_of_structure(node_features),
+                                           get_tensor_of_structure(edge_index).long(),
+                                           get_tensor_of_structure(edge_weight)).detach()
+        approx_mask = np.ones((len(qubo), len(qubo)))
+        node_mean_tensor_list = []
+        for edge_0, edge_1 in zip(edge_index[0], edge_index[1]):
+            node_features_0 = np.array(node_features[edge_0].numpy())
+            node_features_1 = np.array(node_features[edge_1].numpy())
+            node_mean_tensor_list.append(np.mean([node_features_0, node_features_1], axis=0))
+
+        edge_descision_list = self.edge_model.forward(get_tensor_of_structure(node_mean_tensor_list)).detach()
+        for idx, edge_descision in enumerate(edge_descision_list):
+            if edge_descision.detach() <= 0:
+                approx_mask[edge_index[0][idx]][edge_index[1][idx]] = 0
+        return np.multiply(qubo, approx_mask), index
+
+    def get_node_model_and_features(self, problem, qubo):
+        return_node_model = self.node_model
+        return_node_features = get_diagonal_of_qubo(qubo)
+        if 'graph' in problem and 'tsp' in problem:
+            # print('Normalized model choosen!')
+            return_node_model = self.node_model_normalized
+        # return_node_features = get_mean_of_qubo_line(qubo)
+        return return_node_model, return_node_features
+
+    def set_model_weights_from_pygad(self, pygad_chromosome: list):
         model_weights_dict_node = torchga.model_weights_as_dict(model=self.node_model,
                                                                 weights_vector=pygad_chromosome[:self.node_edge_cutoff])
         self.node_model.load_state_dict(model_weights_dict_node)
@@ -38,7 +74,7 @@ class CombinedModel(LearningModel):
                                                                 weights_vector=pygad_chromosome[self.node_edge_cutoff:])
         self.edge_model.load_state_dict(model_weights_dict_edge)
 
-    def get_initial_population(self, population_size):
+    def get_initial_population(self, population_size: int) -> list:
         torch_ga_node = pygad.torchga.TorchGA(model=self.node_model, num_solutions=population_size)
         torch_ga_edge = pygad.torchga.TorchGA(model=self.edge_model, num_solutions=population_size)
         return np.append(torch_ga_node.population_weights, torch_ga_edge.population_weights, axis=-1)
--- evolution_new/pygad_learning.py
+++ evolution_new/pygad_learning.py
@@ -9,6 +9,7 @@ from evolution_utils import get_training_dataset
 
 learning_parameters = {
     'config_name': 'test_evol_m3sat',
+    'training_name': 'M3SAT_128_1_05_10_01_005',
     'population': 100,
     'num_generations': 50,
     'keep_elitism': 5,
@@ -23,31 +24,40 @@ class PygadLearner:
                  fitness_function: Callable[[list, list, list], float]
                  ):
         self.model = model
+        self.training_name = parameters['training_name']
         self.learning_parameters = parameters
         self.config = load_cfg(parameters['config_name'])
         self.fitness_function = fitness_function
         self.best_fitness = 0
         self.avg_fitness_list = []
         self.avg_fitness_generation = 0
-        num_parents_mating = self.learning_parameters['population'] * learning_parameters['percent_of_parents_mating']
-        self.ga_instance = pygad.GA(num_generations=self.learning_parameters['num_generations'],
-                                    # parent_selection_type='rws',
-                                    keep_elitism=self.learning_parameters['keep_elitism'],
-                                    # crossover_type='scattered',
-                                    num_parents_mating=num_parents_mating,
-                                    initial_population=self.model.get_initial_population(
-                                        self.learning_parameters['population']
-                                    ),
-                                    fitness_func=self.get_fitness_function(),
-                                    on_generation=self.callback_generation)
+        self.ga_instance = self.initialize_ga_instance()
 
-    def learn_model(self, model_name):
+    def initialize_ga_instance(self) -> pygad.GA:
+        try:
+            ga_instance = pygad.load(f'{self.training_name}')
+        except FileNotFoundError:
+            num_parents_mating = self.learning_parameters['population'] * \
+                                 learning_parameters['percent_of_parents_mating']
+            ga_instance = pygad.GA(num_generations=self.learning_parameters['num_generations'],
+                                   # parent_selection_type='rws',
+                                   keep_elitism=self.learning_parameters['keep_elitism'],
+                                   # crossover_type='scattered',
+                                   num_parents_mating=num_parents_mating,
+                                   initial_population=self.model.get_initial_population(
+                                       self.learning_parameters['population']
+                                   ),
+                                   fitness_func=self.get_fitness_function(),
+                                   on_generation=self.callback_generation)
+        return ga_instance
+
+    def learn_model(self):
         self.ga_instance.run()
         self.ga_instance.plot_fitness(title="PyGAD & PyTorch - Iteration vs. Fitness")
         # plot_average_fitness(avg_fitness_list)
-        self.ga_instance.save(model_name)
+        self.ga_instance.save(self.training_name)
 
-    def get_fitness_function(self):
+    def get_fitness_function(self) -> Callable[[list, int], float]:
         def fitness_function(solution, solution_idx):
             self.model.set_model_weights_from_pygad(solution)
             problem_dict = get_training_dataset(self.config)
@@ -60,6 +70,7 @@ class PygadLearner:
             self.set_avg_generation_fitness(fitness, solution_idx)
             print(f'Solution {solution_idx}: {fitness}')
             return fitness
+
         return fitness_function
 
     def callback_generation(self):
@@ -73,4 +84,3 @@ class PygadLearner:
 
     def set_avg_generation_fitness(self, fitness: float, solution_id: int):
         self.avg_fitness_generation = (self.avg_fitness_generation * (solution_id - 1) + fitness) / solution_id
-
