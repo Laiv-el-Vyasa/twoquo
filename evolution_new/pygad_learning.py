0c706ae486dedc6b92d79b97d979c91a2132dac1
Laiv el Vyasa <felix@mindt-online.de>
2023-03-02 16:46:06 +0100

    Added time tracking to evaluate multiprocessing

--- evolution_new/pygad_learning.py
+++ evolution_new/pygad_learning.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pygad
-
+from time import time as ttime
 from config import load_cfg
 from learning_model import LearningModel
 from typing import Callable
@@ -18,11 +18,12 @@ class PygadLearner:
         self.model = model
         self.training_name = parameters['training_name']
         self.learning_parameters = parameters
-        self.config = load_cfg(parameters['config_name'])
+        self.config = load_cfg(cfg_id=parameters['config_name'])
         self.get_fitness_value = fitness_function
         self.best_fitness = 0
         self.avg_fitness_list = []
         self.avg_fitness_generation = 0
+        self.avg_duration = []
         self.ga_instance = self.initialize_ga_instance()
 
     def initialize_ga_instance(self) -> pygad.GA:
@@ -54,29 +55,33 @@ class PygadLearner:
 
     def get_fitness_function(self) -> Callable[[list, int], float]:
         def fitness_function(solution, solution_idx):
+            time = ttime()
             self.model.set_model_weights_from_pygad(solution)
             problem_dict = get_training_dataset(self.config)
             qubo_list = problem_dict['qubo_list']
             problem_list = problem_dict['problem_list']
-            solution_list = problem_dict['solution_list]']
+            solution_list = problem_dict['solutions_list']
             approxed_qubo_list = self.model.get_approximation(qubo_list, problem_list)
             fitness = self.get_fitness_value(qubo_list, approxed_qubo_list, solution_list, self.config)
             if fitness > self.best_fitness:
                 self.best_fitness = fitness
             self.set_avg_generation_fitness(fitness, solution_idx)
             print(f'Solution {solution_idx}: {fitness}')
+            self.avg_duration.append(ttime() - time)
             return fitness
 
         return fitness_function
 
-    def callback_generation(self):
-        print("Generation   = {generation}".format(generation=self.ga_instance.generations_completed))
+    def callback_generation(self, ga_instance: pygad.GA):
+        print("Generation   = {generation}".format(generation=ga_instance.generations_completed))
         # print("Fitness      = {fitness}".format(fitness=ga_instance.best_solution()[1]))
         print("Fitness      = {fitness}".format(fitness=self.best_fitness))
         # print(avg_fitness_generation)
         self.avg_fitness_list.append(self.avg_fitness_generation)
         print("Avg. Fitness = {fitness}".format(fitness=self.avg_fitness_generation))
         self.avg_fitness_generation = 0
+        print("Avg. Runtime = {time}".format(time=np.mean(self.avg_duration)))
+        self.avg_duration = []
 
     def set_avg_generation_fitness(self, fitness: float, solution_id: int):
-        self.avg_fitness_generation = (self.avg_fitness_generation * (solution_id - 1) + fitness) / solution_id
+        self.avg_fitness_generation = (self.avg_fitness_generation * (solution_id - 1) + fitness) / (solution_id + 1)
