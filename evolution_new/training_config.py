d1eb8435e6a7be590031fcfacc901e897e873e17
Laiv el Vyasa <felix@mindt-online.de>
2023-03-05 17:05:58 +0100

    Added test config

--- evolution_new/training_config.py
+++ evolution_new/training_config.py
@@ -39,7 +39,26 @@ training_config = {
                 'd': 0.1,
                 'z': 0.05
             }
-        }
+        },
+    'combined_test':
+        {
+            'config_name': 'test_evol_m3sat',
+            'training_name': 'combined_model',
+            'learning_parameters': 'test',
+            'network_type': 'combined',
+            'network_information': {
+                'network_name': 'combinedModelUwU',
+                'node_features': 8,
+            },
+            'fitness_function': 'standard',
+            'fitness_parameters': {
+                'a': 1,
+                'b': 0.5,
+                'c': 10,
+                'd': 0.1,
+                'z': 0.05
+            }
+        },
 }
 
 model_config = {
@@ -54,6 +73,12 @@ learning_parameters_config = {
     'standard': {
         'population': 100,
         'num_generations': 50,
+        'keep_elitism': 5,
+        'percent_of_parents_mating': 0.2
+    },
+    'test': {
+        'population': 10,
+        'num_generations': 5,
         'keep_elitism': 5,
         'percent_of_parents_mating': 0.2
     }
