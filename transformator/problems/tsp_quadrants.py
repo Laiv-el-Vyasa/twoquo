163fde9ed4a689b9fe0156818fa558628f4d1c7b
Laiv el Vyasa <felix@mindt-online.de>
2023-03-19 17:19:01 +0100

    Added TSP QUBO-Generation

--- transformator/problems/tsp_quadrants.py
+++ transformator/problems/tsp_quadrants.py
@@ -3,7 +3,7 @@ from abc import ABC
 import numpy as np
 from numpy import random
 
-from evolution_new.new_visualisation import plot_points
+from evolution_new.new_visualisation import plot_points, qubo_heatmap
 from transformator.common.util import distance_matrix_to_directed_graph
 from transformator.generalizations.graph_based.include_graph_structure import include_graph_structure
 from transformator.problems.problem import Problem
@@ -17,12 +17,17 @@ def get_random_node_number(size: tuple[int, int]) -> int:
     return rng.integers(size[0], size[1] + 1)
 
 
+def get_cities(N: int) -> list[list[float, float]]:
+    random_disorder = get_random_disorder_parameter(N)
+    return [apply_disorder_to_city(random_disorder, city) for city in get_cities_on_circle(N)]
+
+
 def get_cities_on_circle(N: int) -> list[list[float, float]]:
     city_coordinates = []
     radius = N / (2 * np.pi)
     for i in range(N):
         phi = (i / N) * (2 * np.pi)
-        #city_coordinates.append([radius * np.cos((2 * np.pi) / (i + 1)), radius * np.sin((2 * np.pi) / (i + 1))])
+        # city_coordinates.append([radius * np.cos((2 * np.pi) / (i + 1)), radius * np.sin((2 * np.pi) / (i + 1))])
         city_coordinates.append([radius * np.cos(phi), radius * np.sin(phi)])
     # print(city_coordinates)
     # plot_points(city_coordinates)
@@ -47,11 +52,7 @@ def get_random_disorder_parameter(N: int) -> float:
     return random_disorder / np.power(N, scaling_parameter) + critical_disorder
 
 
-def get_distance_matrix(N: int) -> np.ndarray:  # list[list[float]]:
-    print(N)
-    random_disorder = get_random_disorder_parameter(N)
-    print(random_disorder)
-    city_coordinates = [apply_disorder_to_city(random_disorder, city) for city in get_cities_on_circle(N)]
+def get_distance_matrix(N: int, city_coordinates: list[list[float, float]]) -> np.ndarray:  # list[list[float]]:
     # plot_points(city_coordinates)
     dist_matrix = np.zeros((N, N))
     for i in range(N):
@@ -63,47 +64,38 @@ def get_distance_matrix(N: int) -> np.ndarray:  # list[list[float]]:
 
 
 class TSPQuadrants(Problem, ABC):
-    def __init__(self, cfg, dist_matrix, tsp=True, P=10):
+    def __init__(self, cfg, dist_matrix, tsp=True, P=20):
         self.dist_matrix = dist_matrix
-        self.P = 10
+        self.P = P
 
     def gen_qubo_matrix(self):
         n = len(self.dist_matrix)
         Q = np.zeros((n ** 2, n ** 2))
 
-        # quadrants_y = list(range(0, n ** 2, n))
-        # quadrants_x = quadrants_y[1:] + [quadrants_y[0]]
-        #
-        # # The diagonal positive constraints
-        # for start_x in quadrants_y:
-        #     for start_y in quadrants_y:
-        #         for i in range(n):
-        #             Q[start_x + i][start_y + i] = 2 * self.constraint
-        #
-        # # The distance matrices
-        # for (start_x, start_y) in zip(quadrants_x, quadrants_y):
-        #     for i in range(n):
-        #         for j in range(n):
-        #             if i == j:
-        #                 continue
-        #             Q[start_x + i][start_y + j] = self.P * self.dist_matrix[j][i]
-        #         Q[start_x + i][start_y + i] = 2 * self.constraint
-        #
-        # # The middle diagonal negative constraints
-        # for start_x in quadrants_x:
-        #     for i in range(n):
-        #         Q[start_x + i][start_x + i] = -2 * self.constraint
-        #         for j in range(n):
-        #             if i != j:
-        #                 Q[start_x + i][start_x + j] += 2 * self.constraint
+        U = - self.P * np.eye(n) + 2 * self.P * np.triu(np.ones((n, n)), k=1)
 
-        include_graph_structure(
-            qubo_in=Q,
-            graph=distance_matrix_to_directed_graph(self.dist_matrix),
-            positions=n  # ,
-            # score_edge_weights=1.0
-        )
+        # add unary constraint to QUBO
+        for i in range(n):
+            Q[i * n:i * n + n, i * n:i * n + n] += U
 
+        # Set up visitation constraint and add to QUBO
+        for i in range(n - 1, 0, -1):
+            V = np.diag([self.P] * n * i, k=(n - i) * n)
+            Q += V
+
+        # Setting up distance submatrix
+        # D = np.zeros((n_city,n_city))
+        # for r in range(len(dist)):
+        #    D[cnc[r][0],cnc[r][1]] += dist[r]
+        # D = D + D.T
+
+        # add distance submatrices to QUBO
+        for i in range(n - 1):
+            Q[i * n: i * n + n, (i + 1) * n: (i + 1) * n + n] += self.dist_matrix
+        # for the trip back home from the last city
+        Q[:n, -n:] += self.dist_matrix
+
+        qubo_heatmap(Q)
         print(Q)
 
         return Q
@@ -113,7 +105,8 @@ class TSPQuadrants(Problem, ABC):
         problems = []
         for _ in range(n_problems):
             N = get_random_node_number(size)
-            dist_matrix = get_distance_matrix(N)
+            cities = get_cities(N)
+            dist_matrix = get_distance_matrix(N, cities)
             print('Dist matrix: ', dist_matrix)
-            problems.append({"dist_matrix": dist_matrix.tolist(), 'tsp': True})
+            problems.append({"dist_matrix": dist_matrix.tolist(), 'cities': cities, 'tsp': True})
         return problems
