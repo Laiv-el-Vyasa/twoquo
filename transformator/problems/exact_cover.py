7fc8dbf358e99a00ed7d2d773da02c3f3e6e9a77
Laiv el Vyasa <felix@mindt-online.de>
2023-03-27 23:52:27 +0200

    Exact cover Matrix verified

--- transformator/problems/exact_cover.py
+++ transformator/problems/exact_cover.py
@@ -7,6 +7,7 @@ from transformator.problems.problem import Problem
 from transformator.common.util import gen_subsets_matrix
 from transformator.common.util import gen_subset_problems
 import pyqubo
+from pyqubo import *
 
 
 def get_element_number(size: tuple[int, int]) -> int:
@@ -27,8 +28,39 @@ def get_subset_number(m: int, r: float) -> int:
     return int(m / r)
 
 
+# List of elements with subsets they are represented in
 def get_subset_matrix(elements: int, subsets: int) -> list[list[int]]:
     # Create list with three times all elements
+    print('m: ', elements, 'n: ', subsets)
+    element_list = []
+    for i in range(elements):
+        element_list.extend([i, i, i])  # Every Element in three subsets
+    random.shuffle(element_list)
+
+    # Create subset_list
+    subset_list = [[] for _ in range(subsets)]
+    subset_matrix = np.zeros((elements, subsets))
+
+    # Fill each subset with at least one element
+    for i in range(subsets):
+        j = element_list.pop(0)
+        subset_matrix[j][i] = 1
+
+    # Distribute to remaining elements on subsets
+    rng = random.default_rng()
+    for element in element_list:
+        i = rng.integers(0, subsets)
+        while subset_matrix[element][i] == 1:
+            i = rng.integers(0, subsets)
+        subset_matrix[element][i] = 1
+
+    return subset_matrix
+
+
+# List of subsets and the elements they contain
+def get_subset_matrix2(elements: int, subsets: int) -> list[list[int]]:
+    # Create list with three times all elements
+    print('m: ', elements, 'n: ', subsets)
     element_list = []
     for i in range(elements):
         element_list.extend([i, i, i])  # Every Element in three subsets
@@ -54,6 +86,26 @@ def get_subset_matrix(elements: int, subsets: int) -> list[list[int]]:
     return subset_matrix
 
 
+def get_pygubo_matrix(subset_matrix: list[list[int]], n: int, m: int):  # -> list[list[float]]:
+    x = pyqubo.Array.create("x", shape=len(subset_matrix), vartype='BINARY')
+
+    # Constraint one: each variable abgedeckt
+    def f(z):
+        z = 5.25 * z ** 2 - 15.25 * z + 10
+        return z
+
+    H = 4 * sum(f(sum(x[j] * subset_matrix[j][i])) for j in range(n) for i in range(m))
+
+    # Contraint two: as few subsets as possible
+    H = H + sum(x[i] for i in range(n))
+
+    model = H.compile()
+
+    qubo = model.to_qubo()
+
+    print(qubo)
+
+
 class ExactCover(Problem):
     def __init__(self, cfg, subset_matrix, A=2, B=2):
         #print('SubsetMatrix: ' + str(subset_matrix))
@@ -62,7 +114,9 @@ class ExactCover(Problem):
         self.A = n * B + 1
         self.B = B
 
-    def gen_qubo_matrix2(self):
+    def gen_qubo_matrix(self):
+        # get_pygubo_matrix(self.subset_matrix, self.subset_matrix.shape[0], self.subset_matrix.shape[1])
+        print('subsets: ',  self.subset_matrix)
         n = self.subset_matrix.shape[1]
         Q = np.zeros((n, n))
 
@@ -82,10 +136,10 @@ class ExactCover(Problem):
 
             if len(idx) == 1:
                 Q[idx[0]][idx[0]] -= self.A
-
+        #qubo_heatmap(Q)
         return Q
 
-    def gen_qubo_matrix(self):
+    def gen_qubo_matrix2(self):
         subsets = np.array(self.subset_matrix)
         print('subsets: ', subsets)
         M = subsets.shape[0]
@@ -124,6 +178,6 @@ class ExactCover(Problem):
         problems = []
         for i in range(n_problems):
             m = get_element_number(size)
-            n = get_subset_number(m, get_element_subset_factor((0.2, 4.0), 0.62))
+            n = get_subset_number(m, get_element_subset_factor((0.5, 2.0), 0.62))
             problems.append(get_subset_matrix(m, n))
         return [{"subset_matrix": matrix} for matrix in problems]
