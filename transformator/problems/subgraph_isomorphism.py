0a2d2934a48e5fa101123a23a142b0e8842f09ef
Laiv el Vyasa <felix@mindt-online.de>
2023-03-28 17:41:48 +0200

    Phase transitions aware SGI problem generation

--- transformator/problems/subgraph_isomorphism.py
+++ transformator/problems/subgraph_isomorphism.py
@@ -1,7 +1,43 @@
 import itertools
+import math
+
 import numpy as np
+from networkx import erdos_renyi_graph
+
 from transformator.problems.problem import Problem
 from transformator.common.util import gen_graph
+from numpy import random
+
+
+def get_random_node_number(size: list[int, int]) -> int:
+    rng = random.default_rng()
+    return rng.integers(size[0], size[1] + 1)
+
+
+def get_critical_value(n1: int, n2: int, e2: float) -> float:
+    # <Sol> = t_ * e1 ^ (e2 * (n1 over 2)) -> <Sol> = 1
+    t_ = get_possible_assignments(n1, n2)
+    n1_2 = math.comb(n1, 2)
+    e1_critival = np.power((1 / t_), (1 / (n1_2 * e2)))
+    return e1_critival
+
+
+def choose_edge_probability(n1: int, n2: int, e2: float) -> float:
+    e1_critical = get_critical_value(n1, n2, e2)
+    e1 = 0
+    rng = random.default_rng()
+    while not 0 < e1 <1:
+        e1 = rng.normal(e1_critical, 0.2)
+    return e1
+
+
+def get_possible_assignments(n1: int, n2: int) -> int:
+    t_ = 1
+    t = n2
+    while t > n1:
+        t_ *= t
+        t -= 1
+    return t_
 
 
 class SubGraphIsomorphism(Problem):
@@ -55,11 +91,22 @@ class SubGraphIsomorphism(Problem):
 
     @classmethod
     def gen_problems(self, cfg, n_problems, size1, size2, seed=None, **kwargs):
+        graphs1 = []  # Pattern graph
+        graphs2 = []  # Target graph
         # NOTE: size1 should be smaller than size2 or equal to it.
-        graphs1 = gen_graph(n_problems, size1, seed)
+        for i in range(n_problems):
+            n1 = get_random_node_number(size1)
+            n2 = get_random_node_number(size2)
+            e2 = cfg["problems"]["SGI"].get("edge_p2", 0.4)
+            e1 = choose_edge_probability(n1, n2, e2)
+            graphs1.append(erdos_renyi_graph(n1, e1))
+            graphs1.append(erdos_renyi_graph(n2, e2))
+
+
+        #graphs1 = gen_graph(n_problems, size1, seed)
         print('G1 nodes: ', graphs1[0].nodes)
         print('G1 edges: ', graphs1[0].edges)
-        graphs2 = gen_graph(n_problems, size2, seed)
+        #graphs2 = gen_graph(n_problems, size2, seed)
         print('G2 nodes: ', graphs2[0].nodes)
         print('G2 edges: ', graphs2[0].edges)
         return [
